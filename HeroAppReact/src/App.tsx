7cf3db17fd5914dd6d39ae5988aeaff64cd890cc
Likaeus <rodriguezvarona@gmail.com>
2024-03-04 10:46:21 -0500

    deleted unused import

--- HeroAppReact/src/App.tsx
+++ HeroAppReact/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HeroList from "./Components/HeroComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
